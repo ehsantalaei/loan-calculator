0fe238b2cdecc5de53e83b0bc2d5be9d624afde9
ehsantalaei <ehsan.talaei64@gmail.com>
2026-05-24 21:50:23 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'loan-calculator-v1';
+const FILES = [
+  '/loan-calculator/Index.html',
+  '/loan-calculator/manifest.json',
+  '/loan-calculator/icon-192.png',
+  '/loan-calculator/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
